402c57ef1ecfa85d725f80021cb3c53605aed5e8
teewhy-source <edidi.taiye@gmail.com>
2024-03-28 14:16:21 +0100

    added signin component and settinf up firbase utils

--- src/component/routes/Signin/Signin.jsx
+++ src/component/routes/Signin/Signin.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import {signInWithgooglePopup} from "../../../utility-files/firebase/Firebase"
+import {signInWithgooglePopup, createuserdocumentfromauth} from "../../../utility-files/firebase/Firebase"
 const Signin = () => {
     const loggoogleuser = async ()=>{ //asynchronous function becos whenever you u make a call to a database, it has to be asynchronous
-        const response = await signInWithgooglePopup();
-        console.log(response);
+        const {user} = await signInWithgooglePopup();
+       const userdocref = await createuserdocumentfromauth (user)
     }
   return (
     <div>
--- src/utility-files/firebase/Firebase.jsx
+++ src/utility-files/firebase/Firebase.jsx
@@ -7,6 +7,17 @@ import {
   GoogleAuthProvider,
   signInWithPhoneNumber,
 } from "firebase/auth";
+
+//setting up the firestore, firestore is a different service, so we need to import some methods(firestore, doc,getdoc,setdoc) from firestore. below
+import {
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc,
+    Firestore
+} from "firebase/firestore"
+
+
 const firebaseConfig = {
   apiKey: "AIzaSyAaLHGU8ezjimfXt75_UAHV7GEjIckTZpk",
   authDomain: "shopdeygo-90f08.firebaseapp.com",
@@ -27,3 +38,33 @@ provider.setCustomParameters({
 
 export const auth=getAuth()
 export const signInWithgooglePopup =()=> signInWithPopup(auth,provider)//annonymose function, so passed the auth that i created  and provider that i generated up
+export const database = getFirestore ()
+
+export  const createuserdocumentfromauth = async (userAuth) =>{ //this function receives user authentication object
+    const userdocref = doc(database, "users",userAuth.uid)
+    console.log(userdocref);
+
+    const usersnapshot = await getDoc(userdocref) // the snapshot is like a data and it is also a specific kinda object
+    console.log(usersnapshot);
+    console.log(usersnapshot.exists())
+
+    //if user data does not exist == create/set the document with the data from userauth in my collection
+    //if user data exists == ruturn true
+    if (!usersnapshot.exists()) {
+        const {displayName,email} = userAuth
+        const createdate = new Date();
+        try {
+            await setDoc(userdocref,{
+                displayName,
+                email,
+                createdate
+            })
+        } catch (error) {
+            console.log("error creating the user", error.message);
+        }
+
+    }// all this is what is what will happen if user does not exist 
+
+    return userdocref;
+};
+// I NEED TO WATCH VID 76 AGAIN TO GET A BETTER UNDERSTANDING 
