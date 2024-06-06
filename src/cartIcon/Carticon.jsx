d75e3c2161d72b882936c83853b5d249f219f990
teewhy-source <edidi.taiye@gmail.com>
2024-06-06 09:59:28 +0100

    still working on the mobile responsiveness

--- src/cartIcon/Carticon.jsx
+++ src/cartIcon/Carticon.jsx
@@ -1,14 +1,20 @@
-import React from "react";
-import {useContext} from "react"
+import React, { useContext } from "react";
 import { CartContext } from "../context/Cartcontext";
-import shopicon from "../assets/images/shoppingbag.png"
+import { FaTimes } from "react-icons/fa";
+import shopicon from "../assets/images/shoppingbag.png";
 import "./Carticon.styles.scss";
+
 const Carticon = () => {
-    const {iscartopen, setiscartopen, cartcount} = useContext(CartContext)
-    const toggleiscartopen = ()=>setiscartopen(!iscartopen)
+  const { iscartopen, setiscartopen, cartcount } = useContext(CartContext);
+  const toggleiscartopen = () => setiscartopen(!iscartopen);
+
   return (
     <div className="cart-icon-container" onClick={toggleiscartopen}>
-    <img src={shopicon} alt="" className="image" style={{ maxWidth: '3rem', height: '3rem' }} />
+      {iscartopen ? (
+        <FaTimes className="icon" style={{ maxWidth: '3rem', height: '3rem' }} />
+      ) : (
+        <img src={shopicon} alt="Shopping Icon" className="image" style={{ maxWidth: '3rem', height: '3rem' }} />
+      )}
       <span className="item-count">{cartcount}</span>
     </div>
   );
